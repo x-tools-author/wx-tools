a4782a0bd6c9d54719e10d342f74b81f3412afef
x-tools-author <x-tools@outlook.com>
2025-04-27 10:42:11 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-20.04.sh
+++ scripts/ci-build-ubuntu-20.04.sh
@@ -2,4 +2,5 @@
 # The script is run from the root of the repository
 cmake -DCMAKE_BUILD_TYPE=Release -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja"
 cmake --build . --target all
+cmake --build . --target wxTools-appimage
 cmake --build . --target wxTools-deb
