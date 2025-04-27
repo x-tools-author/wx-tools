569d9a007d6778f18678be37ce240d5c0a670152
wuhai <wh@s-track.cn>
2025-04-27 11:49:26 +0800

    chore: update the file

--- cmake/wxWidgets.cmake
+++ cmake/wxWidgets.cmake
@@ -1,12 +1,14 @@
 ﻿set(packet_name "wxWidgets-3.2.8")
 
 set(wxWidget_LIBS ${CMAKE_SOURCE_DIR}/libs/${CMAKE_BUILD_TYPE}/${packet_name})
-set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${wxWidget_LIBS})
-find_package(wxWidgets QUIET COMPONENTS core base aui)
-if(wxWidgets_FOUND AND WXT_AUTO_USING_3RD_LIBS)
+if(EXISTS ${wxWidget_LIBS})
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${wxWidget_LIBS})
+  find_package(wxWidgets QUIET COMPONENTS core base aui)
   include(${wxWidgets_USE_FILE})
+  message(STATUS "wxWidgets found in ${wxWidgets_INCLUDE_DIRS}")
 else()
   # Extract the wxWidgets packet
+  message(STATUS "wxWidgets not found, extracting ${packet_name}...")
   if(NOT EXISTS ${CMAKE_SOURCE_DIR}/3rd/${packet_name}/CMakeLists.txt)
     execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${packet_name}
                     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/3rd)
