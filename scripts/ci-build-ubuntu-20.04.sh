039272adcf8f545ef707b601a3d3e12f42c65e94
x-tools-author <x-tools@outlook.com>
2025-04-27 15:27:24 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-20.04.sh
+++ scripts/ci-build-ubuntu-20.04.sh
@@ -1,6 +1,8 @@
 # The workspace is the root of the repository
 # The script is run from the root of the repository
-cmake -DCMAKE_BUILD_TYPE=Release -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja"
-cmake --build . --target all
-cmake --build . --target wxTools-appimage
-cmake --build . --target wxTools-deb
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja" -S .. -B .
+cmake --build ./build --target all
+cmake --build ./build --target wxTools-appimage
+cmake --build ./build --target wxTools-deb
