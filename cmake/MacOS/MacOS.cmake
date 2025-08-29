f17eea48b7f6227c77ce6095b703e688984c0071
x-tools-author <x-tools@outlook.com>
2025-08-29 15:41:07 +0800

    chore: update the file

--- cmake/MacOS/MacOS.cmake
+++ cmake/MacOS/MacOS.cmake
@@ -12,3 +12,17 @@ function(wxt_make_pkg target dev_id_app dev_id_installer)
   )
   # cmake-format: on
 endfunction()
+
+function(wxt_make_dmg target dev_id_app dev_id_installer)
+  # cmake-format: off
+  add_custom_target(${target}_dmg VERBATIM
+    COMMENT "Making dmg..."
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/assets/${target}
+    COMMAND ${CMAKE_COMMAND} -E echo "Developer ID Application: ${dev_id_app}"
+    COMMAND ${CMAKE_COMMAND} -E echo "Developer ID Installer: ${dev_id_installer}"
+    COMMAND ${CMAKE_COMMAND} -E echo "codesign --deep --force --verbose --sign ${dev_id_app} ${target}.app"
+    COMMAND codesign --deep --force --verbose --sign "${dev_id_app}" "${target}.app"
+    COMMAND hdiutil create -volname "${target}" -srcfolder "${target}.app" -ov -format UDZO "${target}.dmg"
+    COMMAND codesign --force --sign "${dev_id_installer}" "${target}.dmg"
+  )
+  # cmake-format: on
