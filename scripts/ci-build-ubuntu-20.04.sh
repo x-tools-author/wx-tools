22416ae601c428d130eadf229a8b675f77744d2a
x-tools-author <x-tools@outlook.com>
2025-08-29 17:51:59 +0800

    chore: update the file

--- scripts/ci-build-ubuntu-20.04.sh
+++ scripts/ci-build-ubuntu-20.04.sh
@@ -2,7 +2,14 @@
 # The script is run from the root of the repository
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja" -S .. -B .
+
+# first arg
+if [ "$1" == "daily" ]; then
+  cmake -DCMAKE_BUILD_TYPE=Release -DWXT_DAILY_BUILD:BOOL=ON -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja" -S .. -B .
+else
+  cmake -DCMAKE_BUILD_TYPE=Release -DWXT_GENERATE_MSIX:BOOL=OFF -G "Ninja" -S .. -B .
+fi
+
 cmake --build . --target all --parallel 4
 cmake --build . --target wxTools-appimage
 cmake --build . --target wxTools-deb
