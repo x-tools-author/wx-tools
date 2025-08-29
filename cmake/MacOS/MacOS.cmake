e0a97a78ae1ec0f9e6d75d406e8c292be0f7718f
x-tools-author <x-tools@outlook.com>
2025-08-29 15:00:11 +0800

    chore: update the file

--- cmake/MacOS/MacOS.cmake
+++ cmake/MacOS/MacOS.cmake
@@ -6,7 +6,7 @@ function(wxt_make_pkg target dev_id_app dev_id_installer)
     COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/assets/${target}_pkg "||" ${CMAKE_COMMAND} -E true
     COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${CMAKE_BINARY_DIR}/assets/${target}.app ${CMAKE_BINARY_DIR}/assets/${target}_pkg/${target}.app
     COMMAND ${CMAKE_COMMAND} -E echo "codesign --deep --force --verbose --sign "${dev_id_app}" "${target}.app""
-    COMMAND codesign --deep --force --verbose --sign "${dev_id_app}" "${target}.app")
+    COMMAND codesign --deep --force --verbose --sign "${dev_id_app}" "${target}.app"
     COMMENT "Making pkg...")
   # cmake-format: on
 endfunction()
