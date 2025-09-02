1f669961d75bdd887e24dc89e1e2456f2eb45862
x-tools-author <x-tools@outlook.com>
2025-09-02 13:34:12 +0800

    chore: update the file

--- cmake/wxt_3rd_fmt.cmake
+++ cmake/wxt_3rd_fmt.cmake
@@ -1,8 +1,8 @@
 ﻿# https://github.com/fmtlib/fmt
 set(fmt_version "11.2.0")
 set(file_name "fmt-${fmt_version}")
-set(file_url
-    "https://github.com/fmtlib/fmt/releases/download/${fmt_version}/fmt-${fmt_version}.zip")
+set(base_url "https://github.com/fmtlib/fmt")
+set(file_url "${base_url}/releases/download/${fmt_version}/fmt-${fmt_version}.zip")
 
 wxt_download_zip_file(${file_url} ${file_name})
 wxt_auto_import_package(${file_name} fmt)
