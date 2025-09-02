6e7741bd75b1219b999aeb346bca1285a969c485
x-tools-author <x-tools@outlook.com>
2025-09-02 13:32:49 +0800

    chore: update the version to 11.2.0

--- cmake/wxt_3rd_fmt.cmake
+++ cmake/wxt_3rd_fmt.cmake
@@ -1,6 +1,8 @@
 ﻿# https://github.com/fmtlib/fmt
-set(file_name "fmt-11.1.4")
-set(file_url "https://github.com/fmtlib/fmt/releases/download/11.1.4/fmt-11.1.4.zip")
+set(fmt_version "11.2.0")
+set(file_name "fmt-${fmt_version}")
+set(file_url
+    "https://github.com/fmtlib/fmt/releases/download/${fmt_version}/fmt-${fmt_version}.zip")
 
 wxt_download_zip_file(${file_url} ${file_name})
 wxt_auto_import_package(${file_name} fmt)
