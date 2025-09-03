6a9f3a5b42e6a74133eacfec0a85b43d4fd39bab
x-tools-author <x-tools@outlook.com>
2025-09-03 15:01:02 +0800

    chore: update the file

--- cmake/wxt_3rd_glog.cmake
+++ cmake/wxt_3rd_glog.cmake
@@ -1,4 +1,15 @@
 ﻿# https://github.com/google/glog
+
+set(WITH_GTEST
+    OFF
+    CACHE BOOL "Disable Google Test" FORCE)
+set(WITH_GFLAGS
+    OFF
+    CACHE BOOL "Disable Google Flags" FORCE)
+set(BUILD_TESTING
+    OFF
+    CACHE BOOL "Disable building tests" FORCE)
+
 set(glog_version
     0.7.1
     CACHE STRING "glog version")
