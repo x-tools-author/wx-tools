4b9924d10541d2b6b22acc7f49da1fc3227a9c5e
x-tools-author <x-tools@outlook.com>
2025-09-11 16:04:31 +0800

    chore: do something make macos happy

--- cmake/MacOS/MacOS.cmake
+++ cmake/MacOS/MacOS.cmake
@@ -20,8 +20,9 @@ function(wxt_make_dmg target dev_id_app dev_id_installer)
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/assets/${target}
     COMMAND ${CMAKE_COMMAND} -E echo "Developer ID Application: ${dev_id_app}"
     COMMAND ${CMAKE_COMMAND} -E echo "Developer ID Installer: ${dev_id_installer}"
-    COMMAND codesign -f -o runtime -s "${dev_id_app}" -v  ${target}.app --deep
+    COMMAND codesign -f -o runtime -s "${dev_id_app}" -v --deep --entitlements ${WXT_ENTITLEMENTS} ${target}.app
     COMMAND hdiutil create -volname "${target}" -srcfolder "${target}.app" -ov -format UDZO "${WXT_ASSETS_NAME}.dmg"
+    COMMAND codesign -f -s "${dev_id_app}" -v --deep "${WXT_ASSETS_NAME}.dmg"
   )
   # cmake-format: on
 endfunction()
