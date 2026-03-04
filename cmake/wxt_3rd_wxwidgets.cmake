b415fa9c1e424dde1a8564ee29db4c3fab18df98
x-tools-author <x-tools@outlook.com>
2026-03-04 10:47:17 +0800

    chore: update version to 3.3.2

--- cmake/wxt_3rd_wxwidgets.cmake
+++ cmake/wxt_3rd_wxwidgets.cmake
@@ -1,5 +1,5 @@
 ﻿set(wxWidgets_version
-    3.3.1
+    3.3.2
     CACHE STRING "wxWidgets version")
 set(file_name "wxWidgets-${wxWidgets_version}")
 set(base_url "https://github.com/wxWidgets/wxWidgets/releases/download")
